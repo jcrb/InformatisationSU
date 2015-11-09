ea65adf2aff411036dcf02b3e2e3cf06b78a5794
JCB <jeanclaude.bartier@gmail.com>
2015-11-09 11:45:41 +0100

    maj

--- Septembre2015/routines.R
+++ Septembre2015/routines.R
@@ -10,14 +10,20 @@
 #' @usage a <- summary.par.logiciel(d$MS_exhaus, d$Logiciel_2015)
 
 summary.par.logiciel <- function(vx, fx){
+  q25 <- function(x){quantile(x, probs=0.25, na.rm = TRUE)}
+  q75 <- function(x){quantile(x, probs=0.75, na.rm = TRUE)}
+
   min <- tapply(vx, fx, min, na.rm = TRUE)
   max <- tapply(vx, fx, max, na.rm = TRUE)
   mean <- tapply(vx, fx, mean, na.rm = TRUE)
+  mediane <- tapply(vx, fx, median, na.rm = TRUE)
   sd <- tapply(vx, fx, sd, na.rm = TRUE)
   n  <- tapply(vx, fx, length)
+  Q25 <- tapply(vx, fx, q25)
+  Q75 <- tapply(vx, fx, q75)
   
-  a <- cbind(min, max, mean, sd, n)
-  colnames(a) <- c("Min", "Max", "moyenne", "ecart-type", "Nb")
+  a <- cbind(min, max, mean, mediane, sd, n, Q25, Q75)
+  colnames(a) <- c("Min", "Max", "moyenne", "médiane", "ecart-type", "Nb", "Q25", "Q75")
   
   return(a)
 }
